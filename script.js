5db8aea3b83fe00c5d46324b13709a5228267e97
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-02-22 22:52:30 -0300

    entrega requisito 5 e 6

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ let cartaGerada = document.querySelector('#carta-gerada');
 let inputTexto = document.querySelector('#carta-texto');
 
 botaoCriar.addEventListener('click', function() {
+    if (inputTexto.value.trim().length === 0) {
+        let mensagem = 'Por favor, digite o conteúdo da carta.';
+        cartaGerada.innerHTML = mensagem;
+        /* cartaGerada.appendChild(cartaGerada); */
+    } else {
     let palvrasSeparadas = inputTexto.value.split(" ");
     for (let i = 0; i < palvrasSeparadas.length; i += 1) {
         let tagSpan = document.createElement('span');
         tagSpan.innerHTML = `${palvrasSeparadas[i]}`;
+        /* cartaGerada.innerHTML = tagSpan */
 
         cartaGerada.appendChild(tagSpan);
+    }
     }
     console.log(cartaGerada);
 })
