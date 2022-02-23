2c631b47d191dad4a5979b7d5ad61fb7762f2011
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-02-23 13:23:11 -0300

    entrega requisito 16

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let botaoCriar = document.querySelector('#criar-carta');
 let cartaGerada = document.querySelector('#carta-gerada');
 let inputTexto = document.querySelector('#carta-texto');
+let grupo1 = ['newspaper', 'magazine1', 'magazine2'];
+let grupo2 = ['medium', 'big', 'reallybig'];
+let grupo3 = ['rotateleft', 'rotateright'];
+let grupo4 = ['skewleft', 'skewright'];
+function sorteia(arr) {
+    var rand = Math.floor(Math.random() * arr.length);
+    return arr[rand];
+}
 
 botaoCriar.addEventListener('click', function() {
     if (inputTexto.value.trim().length === 0) {
@@ -8,14 +16,20 @@ botaoCriar.addEventListener('click', function() {
         cartaGerada.innerHTML = mensagem;
         /* cartaGerada.appendChild(cartaGerada); */
     } else {
+        let classe1 = sorteia(grupo1);
+        let classe2 = sorteia(grupo2);
+        let classe3 = sorteia(grupo3);
+        let classe4 = sorteia(grupo4);
     let palvrasSeparadas = inputTexto.value.split(" ");
     for (let i = 0; i < palvrasSeparadas.length; i += 1) {
         let tagSpan = document.createElement('span');
         tagSpan.innerHTML = `${palvrasSeparadas[i]}`;
+        tagSpan.className = classe1 + ' ' + classe2 + ' ' + classe3 + ' ' + classe4;
         /* cartaGerada.innerHTML = tagSpan */
 
         cartaGerada.appendChild(tagSpan);
     }
     }
-    console.log(cartaGerada);
+    /* console.log(cartaGerada); */
 })
+/* https://pt.stackoverflow.com/questions/73214/criar-uma-combina%C3%A7%C3%A3o-aleatoria-com-classes-css-utilizando-javascript-ou-jquery */
